c162ee121055b194b2d40ccf4ca6953aad36c9b3
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2026-05-16 15:22:53 +0530

    chore: clean up unwrap and enhance logging

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::Path};
 
-use typst::diag::SourceDiagnostic;
+use typst::{
+    diag::SourceDiagnostic,
+    syntax::{FileId, VirtualPath},
+};
 use typst_as_lib::TypstEngine;
 
 fn main() {
-    let src = PathBuf::from("./templates/main.typ");
-    typst_to_html(src, "./templates/");
+    let typst_engine = TypstEngine::builder()
+        .with_file_system_resolver("./templates/")
+        .build();
+    let src = Path::new("./templates/main.typ");
+    typst_to_html(&typst_engine, src);
 }
 
-fn typst_to_html(filepath: PathBuf, template_dir: &str) {
-    let template = TypstEngine::builder()
-        .with_file_system_resolver(template_dir)
-        .build();
+fn typst_to_html(engine: &TypstEngine, filepath: &Path) {
     let filename = filepath
         .file_name()
         .expect("file path is root or a directory")
         .to_str()
         .expect("filename not in unicode");
-    let doc = template.compile::<_, typst_html::HtmlDocument>(filename);
+    let doc = engine.compile::<_, typst_html::HtmlDocument>(filename);
     doc.warnings
         .iter()
         .for_each(|srcdiag| handle_source_diagnostic(srcdiag));
 
     match doc.output {
         Ok(document) => {
-            let html = typst_html::html(&document)
-                .unwrap_or_else(|_| unreachable!("typst_html::html never returns an error"));
+            let html = match typst_html::html(&document) {
+                Ok(html_out) => html_out,
+                Err(_) => unreachable!("typst_html::html doesn't return error"),
+            };
             match fs::write(filepath.with_extension("html"), html) {
                 Ok(_) => (),
                 Err(html_err) => eprintln!("Failed to write html because of {html_err}"),
@@ -41,6 +46,13 @@ fn handle_source_diagnostic(diagnostic: &SourceDiagnostic) {
         typst::diag::Severity::Warning => "WARN",
     };
     let message = &diagnostic.message;
+    let file = &diagnostic
+        .span
+        .id()
+        .unwrap_or(FileId::new_fake(VirtualPath::new(".")))
+        .vpath()
+        .as_rootless_path()
+        .display();
     let hints = diagnostic.hints.join("\n");
-    eprintln!("[{level}] {message}\n{hints}")
+    eprintln!("[{level}] {file} {message}\n{hints}")
 }
